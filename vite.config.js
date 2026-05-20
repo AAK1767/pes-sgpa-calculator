9572ebd0dbb4c99b6327143419f2ee5215c779f8
AAK1767 <arhamahmed807@gmail.com>
2026-05-20 15:16:32 +0530

    sw fixed 2

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
       strategies: 'injectManifest',
       srcDir: 'src',
       filename: 'sw.js',
+      injectManifest: {
+        globIgnores: ['**/index.html'],
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'PESU Grade Calculator',
